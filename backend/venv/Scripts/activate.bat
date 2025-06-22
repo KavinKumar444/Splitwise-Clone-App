6b48bdc7ed6a42b1591500ec84fc57fce3345d61
KavinKumar444 <kavinkumarp12@gmail.com>
2025-06-22 13:28:15 +0530

    Initial commit - full working Splitwise Clone

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\downloads\FS\backend\venv
+set VIRTUAL_ENV=C:\Users\kavin\Splitwise-Clone\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
